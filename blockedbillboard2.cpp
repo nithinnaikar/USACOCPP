0e4a25cef39fefb99f4085aefb1001e537a321bc
nithinnaikar <62145888+nithinnaikar@users.noreply.github.com>
2022-09-10 18:23:15 -0400

    Add files via upload

---
+++ blockedbillboard2.cpp
@@ -0,0 +1,41 @@
+// Source: https://usaco.guide/general/io
+
+#include <fstream>
+using namespace std;
+
+int main(){
+
+	ifstream fin("billboard.in");
+	ofstream fout("billboard.out");
+
+	int lawnMowerBLX, lawnMowerBLY, lawnMowerTRX, lawnMowerTRY;
+	int cowFeedBLX, cowFeedBLY, cowFeedTRX, cowFeedTRY;
+
+	fin >> lawnMowerBLX >> lawnMowerBLY >> lawnMowerTRX >> lawnMowerTRY;
+	fin >> cowFeedBLX >> cowFeedBLY >> cowFeedTRX >> cowFeedTRY;
+	
+	int lawnMowerWidth = lawnMowerTRX - lawnMowerBLX;
+	int lawnMowerLength = lawnMowerTRY - lawnMowerBLY;
+
+	int intersectionWidth = min(lawnMowerTRX, cowFeedTRX) - max(lawnMowerBLX, cowFeedBLX);
+	int intersectionLength = min(lawnMowerTRY, cowFeedTRY) - max(lawnMowerBLY, cowFeedBLY);
+
+	if (intersectionWidth > 0 && intersectionLength > 0){
+		if (intersectionWidth == lawnMowerWidth || intersectionLength == lawnMowerLength){
+			int tarpArea = (lawnMowerWidth * lawnMowerLength) - (intersectionWidth * intersectionLength);
+			fout << tarpArea;
+		}
+		else{
+			int tarpArea = (lawnMowerWidth * lawnMowerLength);
+			fout << tarpArea;
+		}
+		
+		
+	}
+	else{
+		int tarpArea = (lawnMowerWidth * lawnMowerLength);
+		fout << tarpArea;
+	}
+
+	
+}
